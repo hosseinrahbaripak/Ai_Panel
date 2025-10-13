03e6916add26b741f168a2f1eca58438d1b157f8
hosseinrahbaripak <hossein.rp377@gmail.com>
2025-10-13 13:46:43 +0330

    fix change

--- Ai_Panel/Pages/Admin/AiConfig/Index.cshtml.cs
+++ Ai_Panel/Pages/Admin/AiConfig/Index.cshtml.cs
@@ -6,14 +6,25 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace Ai_Panel.Pages.Admin.AiConfig
 {
     [PermissionChecker]
-    public class IndexModel(IAiConfigRepository aiConfigRepository) : PageModel
+    public class IndexModel : PageModel
     {
+        private readonly IAiConfigRepository _aiConfigRepository;
 
-        //public List<Domain.AiConfig> AiConfigs { get; set; }
+        public IndexModel(IAiConfigRepository aiConfigRepository)
+        {
+            _aiConfigRepository = aiConfigRepository;
+        }
+
+        public List<Domain.AiConfig> AiConfigs { get; set; }
 
         public async Task<IActionResult> OnGet()
         {
-            //AiConfigs = await aiConfigRepository.GetAll(x => !x.IsDelete,x=>x.OrderByDescending(o=>o.Id), "AiModel,AiModel.Parent");
+            AiConfigs = await _aiConfigRepository.GetAll(
+                x => !x.IsDelete,
+                x => x.OrderByDescending(o => o.Id),
+                "AiModel,AiModel.Parent"
+            );
+
             return Page();
         }
     }
