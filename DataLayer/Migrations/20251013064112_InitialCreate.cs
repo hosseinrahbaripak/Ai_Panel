76880c49fcd938031b1dc66de61decd86783b9b2
hosseinrahbaripak <hossein.rp377@gmail.com>
2025-10-13 10:13:13 +0330

    fix migrations

---
+++ DataLayer/Migrations/20251013064112_InitialCreate.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Ai_Panel.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AiModels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    ParentId = table.Column<int>(type: "int", nullable: true),
+                    IsDelete = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AiModels", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AiModels_AiModels_ParentId",
+                        column: x => x.ParentId,
+                        principalTable: "AiModels",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AiPlatforms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ApiEndpoint = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsDelete = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AiPlatforms", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ErrorLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Message = table.Column<string>(type: "nvarchar(1500)", maxLength: 1500, nullable: false),
+                    InnerMessage = table.Column<string>(type: "nvarchar(1500)", maxLength: 1500, nullable: true),
+                    Token = table.Column<Guid>(type: "uniqueidentifier", maxLength: 50, nullable: false),
+                    Action = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    DateTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ErrorLogs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    RoleId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoleTitle = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    ParentId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.RoleId);
+                    table.ForeignKey(
+                        name: "FK_Roles_Roles_ParentId",
+                        column: x => x.ParentId,
+                        principalTable: "Roles",
+                        principalColumn: "RoleId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(350)", maxLength: 350, nullable: true),
+                    FirstName = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: true),
+                    ActiveCode = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: false),
+                    MobileNumber = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    PassKey = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    DateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdateDateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDelete = table.Column<bool>(type: "bit", nullable: false),
+                    HasAccessToAiChat = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AiConfigs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Version = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    AiPlatformId = table.Column<int>(type: "int", nullable: true),
+                    AiModelId = table.Column<int>(type: "int", nullable: false),
+                    Prompt = table.Column<string>(type: "nvarchar(2000)", maxLength: 2000, nullable: false),
+                    Temperature = table.Column<float>(type: "real", nullable: false),
+                    TopP = table.Column<float>(type: "real", nullable: false),
+                    MaxTokens = table.Column<int>(type: "int", nullable: false),
+                    Stop = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    N = table.Column<int>(type: "int", nullable: false),
+                    PresencePenalty = table.Column<float>(type: "real", nullable: false),
+                    FrequencyPenalty = table.Column<float>(type: "real", nullable: false),
+                    DateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdateDateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreateBy = table.Column<int>(type: "int", nullable: true),
+                    UpdateBy = table.Column<int>(type: "int", nullable: true),
+                    IsDelete = table.Column<bool>(type: "bit", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AiConfigs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AiConfigs_AiModels_AiModelId",
+                        column: x => x.AiModelId,
+                        principalTable: "AiModels",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AiConfigs_AiPlatforms_AiPlatformId",
+                        column: x => x.AiPlatformId,
+                        principalTable: "AiPlatforms",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AiModelAiPlatform",
+                columns: table => new
+                {
+                    ModelsId = table.Column<int>(type: "int", nullable: false),
+                    PlatformsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AiModelAiPlatform", x => new { x.ModelsId, x.PlatformsId });
+                    table.ForeignKey(
+                        name: "FK_AiModelAiPlatform_AiModels_ModelsId",
+                        column: x => x.ModelsId,
+                        principalTable: "AiModels",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AiModelAiPlatform_AiPlatforms_PlatformsId",
+                        column: x => x.PlatformsId,
+                        principalTable: "AiPlatforms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ApiRequestLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Header = table.Column<string>(type: "nvarchar(2000)", maxLength: 2000, nullable: true),
+                    Url = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Body = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Response = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UserId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApiRequestLogs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ApiRequestLogs_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserSessions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    Token = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    DateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateLogout = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsLogout = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserSessions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserSessions_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserAiChatLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserMessage = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    AiCouldResponse = table.Column<bool>(type: "bit", nullable: false),
+                    AiResponse = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SummarizationCost = table.Column<double>(type: "float", nullable: false),
+                    RequestCost = table.Column<double>(type: "float", nullable: false),
+                    EmbeddingCost = table.Column<double>(type: "float", nullable: false),
+                    AiConfigId = table.Column<int>(type: "int", nullable: true),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserAiChatLogs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserAiChatLogs_AiConfigs_AiConfigId",
+                        column: x => x.AiConfigId,
+                        principalTable: "AiConfigs",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_UserAiChatLogs_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AiModels",
+                columns: new[] { "Id", "IsDelete", "ParentId", "Title" },
+                values: new object[,]
+                {
+                    { 1, false, null, "Open Ai" },
+                    { 8, false, null, "groq" },
+                    { 2, false, 1, "gpt-4o" },
+                    { 3, false, 1, "gpt-4o-mini" },
+                    { 4, false, 1, "o1" },
+                    { 5, false, 1, "o1-mini" },
+                    { 6, false, 1, "o3-mini" },
+                    { 7, false, 1, "gpt-3.5-turbo" },
+                    { 9, false, 8, "deepseek-r1-distill-llama-70b" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AiConfigs_AiModelId",
+                table: "AiConfigs",
+                column: "AiModelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AiConfigs_AiPlatformId",
+                table: "AiConfigs",
+                column: "AiPlatformId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AiModelAiPlatform_PlatformsId",
+                table: "AiModelAiPlatform",
+                column: "PlatformsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AiModels_ParentId",
+                table: "AiModels",
+                column: "ParentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApiRequestLogs_UserId",
+                table: "ApiRequestLogs",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Roles_ParentId",
+                table: "Roles",
+                column: "ParentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAiChatLogs_AiConfigId",
+                table: "UserAiChatLogs",
+                column: "AiConfigId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAiChatLogs_UserId",
+                table: "UserAiChatLogs",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserSessions_UserId",
+                table: "UserSessions",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AiModelAiPlatform");
+
+            migrationBuilder.DropTable(
+                name: "ApiRequestLogs");
+
+            migrationBuilder.DropTable(
+                name: "ErrorLogs");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+
+            migrationBuilder.DropTable(
+                name: "UserAiChatLogs");
+
+            migrationBuilder.DropTable(
+                name: "UserSessions");
+
+            migrationBuilder.DropTable(
+                name: "AiConfigs");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "AiModels");
+
+            migrationBuilder.DropTable(
+                name: "AiPlatforms");
+        }
+    }
+}
