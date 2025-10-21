be33fb6850c9243fe0cb1e0dc4a8a8d8398be3e0
hosseinrahbaripak <hossein.rp377@gmail.com>
2025-10-21 21:16:57 +0330

    fix get aiConfigs

---
+++ Ai_Panel.Application/DTOs/AiConfig/AiConfigGroupDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ai_Panel.Application.DTOs.AiConfig
+{
+    public class AiConfigGroupDto
+    {
+        public int GroupId { get; set; }
+        public string Title { get; set; }
+        public List<string> Modles { get; set; } = new();
+    }
+}
--- Ai_Panel/Pages/Admin/MultiStepAiConfig/Index.cshtml.cs
+++ Ai_Panel/Pages/Admin/MultiStepAiConfig/Index.cshtml.cs
@@ -1,4 +1,5 @@
-using Ai_Panel.Application.Contracts.Persistence.EfCore;
+﻿using Ai_Panel.Application.Contracts.Persistence.EfCore;
+using Ai_Panel.Application.DTOs.AiConfig;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -9,14 +10,16 @@ namespace Ai_Panel.Pages.Admin.MultiStepAiConfig
     public class IndexModel : PageModel
     {
         private readonly IAiConfigRepository _aiConfigRepository;
+        private readonly IGenericRepository<Domain.AiConfigGroup> _aiConfigGroup;
 
-        public IndexModel(IAiConfigRepository aiConfigRepository)
+        public IndexModel(IAiConfigRepository aiConfigRepository , IGenericRepository<Domain.AiConfigGroup> aiConfigGroup)
         {
             _aiConfigRepository = aiConfigRepository;
+            _aiConfigGroup = aiConfigGroup;
         }
 
         public List<Domain.AiConfig> AiConfigs { get; set; }
-
+        public List<AiConfigGroupDto> AiConfigGroup { get; set; } 
         public async Task<IActionResult> OnGet()
         {
             AiConfigs = await _aiConfigRepository.GetAll(
@@ -24,8 +27,20 @@ namespace Ai_Panel.Pages.Admin.MultiStepAiConfig
                 x => x.OrderByDescending(o => o.Id),
                 "AiModel,AiModel.Parent"
             );
+            AiConfigGroup = AiConfigs
+                .Where(x => x.AiConfigGroupId.HasValue)
+                .GroupBy(x => x.AiConfigGroupId.Value)
+                .Select(g => new AiConfigGroupDto
+                {
+                    GroupId = g.Key,
+                    Title = g.First().Title,
+                    Modles = g.Select(x => x.AiModel.Title).ToList(),
+                })
+                .ToList();
+
 
             return Page();
         }
     }
+
 }
